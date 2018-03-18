d3bcfeb6576ff0e2112ce8f7b9b4444869e1519d
ANdrey Nikishaev <creotiv@gmail.com>
2018-03-18 23:09:18 +0200

    fix

--- dump.py
+++ dump.py
@@ -1,14 +1,18 @@
 import sys
+import os
 import time
 from base import MiBand2
 
 MAC = sys.argv[1]
-fp = open(sys.argv[2], 'a')
+filepath = sys.argv[2]
+if os.path.exists(sys.argv[2]):
+    os.remove(sys.argv[2])
+fp = open(filepath, 'a')
 fp.write('time, heartrate\n')
 
 
 def log(rate):
-    data = "%s, %s" % (int(time.time()), rate)
+    data = "%s, %s\n" % (int(time.time()), rate)
     fp.write(data)
     print data
 
